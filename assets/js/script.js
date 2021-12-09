d4104ec9eacf94e5fcfcfa61a111fc713faac3c1
Harshit Harkhani <71262140+HarshitHarkhani99@users.noreply.github.com>
2021-12-09 12:46:09 +0530

    Html Template

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+// Header
+$('.toggle-menu').click(function () {
+    $('.sidebar').addClass('active');
+    $('.close-sidebar').addClass('active');
+})
+$('.close-sidebar').click(function () {
+    $('.sidebar').removeClass('active');
+    $(this).removeClass('active');
+})
+$(document).keyup(function(e) {
+    $('.sidebar').removeClass('active');
+    $('.close-sidebar').removeClass('active');
+});
